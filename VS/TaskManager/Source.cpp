b240e219366acac25786594d844faf15f77a8f7b
RenDev <menertson@gmail.com>
2020-06-02 23:01:48 -0700

    Refactorisation
    Refactored the data structure for the classes so that i could wrap up  100's of lines of code. Made it so you could pass i n any object and it would read its discription and check if it was clicked instead of manually checking for every object type for each gamestate and then checking if you were clicking it. This saved an estimated 200 lines give or take because other functions benifited from the more simplistic refacorisation.

--- VS/TaskManager/Source.cpp
+++ VS/TaskManager/Source.cpp
@@ -7,56 +7,59 @@
 fakeclass sbh;
 unsigned int gamestate=1;
 string currentModeTitle;
+string newstr="";
 bool update=0;
+int sel=0;
+bool selst = 0;
 unsigned int bIndex[3] = {0,0,0};
 unsigned int returnGamestate;
-/*void conHelp(olc::PixelGameEngine* pge, float x, float y, int b = -1,  int b1 = -1, int b2 = -1) {
-	int mode=1;
-	if (b == -1) {
-		b = 0;
-	}
-	if (b1 == -1) {
-		b1 = 0;
-	}
-	if (b != -1 && b1 != -1 &&b2 ==-1) {
-		b2 = 0;
-		mode = 2;
-	}
-	if (b != -1 && b1 != -1 && b2 != -1) {
-		mode = 3;
-	}
-	const unsigned int sizes[3] = { sbh.data.ProjectDatabase.size(),sbh.data.ProjectDatabase[b].projectDat.size(),sbh.data.ProjectDatabase[b].projectDat[b1].tasks.size() };
-	for (unsigned int i = 0; i < sizes[mode-1]; i++) {
-		int tmpint = i;
-		olc::vi2d pos = { 22,22 + (tmpint * 26) };
-		if (x > pos.x&& x < pos.x + 71 && y>pos.y&& y < pos.y + 25) {
-			bIndex[mode-1] = mode;
-			switch (mode) {
-			case 1:
-				sbh.discription(100, 20, sbh.data.ProjectDatabase[i]);
-				break;
-			case 2:
-				sbh.discription(100, 20, sbh.data.ProjectDatabase[b].projectDat[i]);
-				break;
-			case 3:
-				sbh.discription(100, 20, sbh.data.ProjectDatabase[b].projectDat[b1].tasks[i]);
-				break;
-			//case 3:
-			//	sbh.discription(100,20,sbh.data.ProjectDatabase[b].projectDat[b1].tasks)
-			//	break;
-				//sbh.discription(100, 20, sbh.data.ProjectDatabase[i]);
+void updat(int val) {
+	gamestate = val;
+	update = 1;
+}
+void menuCollision(olc::PixelGameEngine* pge) {
+	float my = pge->GetMouseY();
+	if (!selst || my>250) {
+		float mx = pge->GetMouseX();
+		for (int i = 0; i < 3; i++) {
+			if (mx > 20 + (i * 90) && mx < 95 + (i * 90) && my > 250) {
+				sel = i + 2;
 			}
-			if (pge->GetMouse(0).bPressed) {
-				gamestate = mode-1;
-				update = 1;
-			}
-		}
-		else {
-			pge->FillRect({ 100,20 }, { 181,140 });
 		}
 	}
 }
-*/
+template <class U>
+void conHelp(olc::PixelGameEngine* pge, U *obj,float mx, float my,short unsigned int mode) {
+	bool st = 0;
+	for (unsigned int i = 0; i <obj->Dat.size(); i++) {
+		int tmpint = i;
+		olc::vi2d pos = { 22,22 + (tmpint * 26) };
+		if (mx > pos.x&& mx < pos.x + 71 && my>pos.y&& my < pos.y + 25) {
+			bIndex[int(mode-1)] = i;
+			sbh.discription(100, 20, obj->Dat[i]);
+			date c=obj->Dat[i].created;
+			date m = obj->Dat[i].modified;
+			sbh.drawTime(&c, 0);
+			sbh.drawTime(&m, 1);
+			st = 1;
+			if (pge->GetMouse(0).bPressed) {
+				if (sel == 0) {
+					updat(mode + 1);
+				}
+				else {
+					returnGamestate = mode;
+					updat(sel);
+				}
+			}
+		}
+		else {
+			if (!st) {
+				pge->FillRect({ 101,31 }, { 170,100 });
+				pge->FillRect({ 20,210 }, { 300,30 }, olc::WHITE);
+			}
+		}
+	}
+}
 void trayMenu(olc::PixelGameEngine* pge,float y, int mode) {
 	string title[3] = {"Project","Grouping","Task"};
 	string option[3] = { "New","Edit","Delete" };
@@ -71,22 +74,9 @@ void trayMenu(olc::PixelGameEngine* pge,float y, int mode) {
 			}
 		}
 }
+void textIn() {
 
-int menuCollision(olc::PixelGameEngine *pge,float mx,float my) {
-	if (pge->GetMouse(0).bPressed) {
-		for (int i = 0; i < 3; i++) {
-			if (mx > 20 + (i * 90) && mx < 95 + (i * 90) && my > 250) {
-				return i;
-			}
-		}
-	}
-	else { return -1; }
 }
-
-template <class U>
-void options(olc::PixelGameEngine* pge, U obj) {//TODO FINSISH THIS FUNCTION
-	//TODO FINSISH THIS FUNCTION
-} //TODO FINSISH THIS FUNCTION
 
 class Example : public olc::PixelGameEngine
 {
@@ -113,95 +103,45 @@ public:
 			switch (gamestate) {
 			case 1:
 				sbh.listProjects(20, 20, 0, "Projects");
+				menuCollision(this);
 				trayMenu(this, 250, 0);
 				break;
 			case 2:
 				sbh.listRegion(20, 20, 0, "Groupings", bIndex[0]);
+				menuCollision(this);
 				trayMenu(this, 250, 1);
 				break;
 			case 3:
 				sbh.listTasks(20, 20, "Tasks", bIndex[0],bIndex[1]);
+				menuCollision(this);
 				trayMenu(this, 250, 2);
 				break;
 			case 4:
 				sbh.taskVeiw(bIndex[0], bIndex[1], bIndex[2]);
-				//trayMenu(this, 250, 0);
 				break;
+			case 5:
+				olc::PixelGameEngine::DrawString(125, 0, "N e w");
+				olc::PixelGameEngine::DrawString(130, 6, "______");
+					break;
+
 			default:
 				gamestate = 1;
 			}
 		}
 		float mx = GetMouseX();
 		float my = GetMouseY();
-		int sel=menuCollision(this, mx, my);
-		switch (gamestate) {
+			switch (gamestate) {
 			case 1:
-				for (unsigned int i = 0; i < sbh.data.Dat.size(); i++) {
-					int tmpint = i;
-					olc::vi2d pos = { 22,22 + (tmpint * 26) };
-					if (mx > pos.x&& mx < pos.x + 71 && my>pos.y&& my < pos.y + 25) {
-						bIndex[0] = i;
-						sbh.discription(100, 20, sbh.data.Dat[i]);
-						sbh.drawTime(&sbh.data.Dat[i].created, 0);
-						sbh.drawTime(&sbh.data.Dat[i].modified, 1);
-						if (GetMouse(0).bPressed) {
-							gamestate = 2;
-							update = 1;
-						}
-					}
-					else {
-						olc::PixelGameEngine::FillRect({ 101,31 }, { 170,100 });
-						olc::PixelGameEngine::FillRect({ 20,210 }, { 300,30 }, olc::WHITE);
-					}
-				}
-				/*if (sel != -1) {
-					returnGamestate = 1;
-					gamestate = 5;
-					update = 1;
-				}*/
+				conHelp(this, &sbh.data, mx, my, 1);
 				break;
 			case 2:
-				for (unsigned int i = 0; i < sbh.data.Dat[bIndex[0]].Dat.size(); i++) {
-					int tmpint = i;
-					olc::vi2d pos = { 22,22 + (tmpint * 26) };
-					if (mx > pos.x&& mx < pos.x + 71 && my>pos.y&& my < pos.y + 25) {
-						bIndex[1] = i;
-						sbh.discription(100, 20, sbh.data.Dat[bIndex[0]].Dat[i]);
-						sbh.drawTime(&sbh.data.Dat[bIndex[0]].Dat[i].created, 0);
-						sbh.drawTime(&sbh.data.Dat[bIndex[0]].Dat[i].modified, 1);
-						if (GetMouse(0).bPressed) {
-							gamestate = 3;
-							update = 1;
-						}
-					}
-					else {
-						olc::PixelGameEngine::FillRect({ 101,31 }, { 165,100 });
-						olc::PixelGameEngine::FillRect({ 20,210 }, { 300,30 });
-					}
-				}
+				conHelp(this, &sbh.data.Dat[bIndex[0]], mx, my, 2);
 				break;
 			case 3:
-				for (unsigned int i = 0; i < sbh.data.Dat[bIndex[0]].Dat[bIndex[1]].Dat.size(); i++) {
-					int tmpint = i;
-					olc::vi2d pos = { 22,22 + (tmpint * 26) };
-					if (mx > pos.x&& mx < pos.x + 71 && my>pos.y&& my < pos.y + 25) {
-						bIndex[1] = i;
-						sbh.discription(100, 20, sbh.data.Dat[bIndex[0]].Dat[bIndex[1]].Dat[i]);
-						sbh.drawTime(&sbh.data.Dat[bIndex[0]].Dat[bIndex[1]].Dat[i].created, 0);
-						sbh.drawTime(&sbh.data.Dat[bIndex[0]].Dat[bIndex[1]].Dat[i].modified, 0);
-						if (GetMouse(0).bPressed) {
-							gamestate = 4;
-							update = 1;
-						}
-					}
-					else {
-						olc::PixelGameEngine::FillRect({ 101,31 }, { 165,100 });
-						olc::PixelGameEngine::FillRect({ 20,210 }, { 300,30 });
-					}
-				}
+				conHelp(this, &sbh.data.Dat[bIndex[0]].Dat[bIndex[1]], mx, my, 3);
 				break;
 			case 4:
-				sbh.discription(100, 20, sbh.data.Dat[bIndex[0]].Dat[bIndex[1]].Dat[bIndex[1]]);
+				sbh.discription(100, 20, sbh.data.Dat[bIndex[0]].Dat[bIndex[1]].Dat[bIndex[2]]);
 				break;
 			case 5:
 				break;
